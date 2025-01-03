2f9915587526ce7f66c5a7dad31449fdb77dc267
unknown <kushalthakkar2024@gmail.com>
2025-01-03 20:06:18 +0530

    completed til page 6

--- script.js
+++ script.js
@@ -28,15 +28,19 @@ function navAnimation() {
           duration: 0.5,
           height: "17vh",
         });
-        enterTL.to(".navelem h5", {
-          display: "block",
+        enterTL.to(".navelem h5",{
+          display:"block",
+          duration:0,
+        })
+        enterTL.to(".navelem h5 span", {
+          display: "inline-block",
           opacity: 1,
           y: 0,
           stagger: {
-            amount: 0.4,
+            amount: 0.7,
           },
           duration: 1,
-          ease: "back.out(1.4)",
+          // ease: "back.out(1.4)",
         });
       } else {
         // jo mouse leave kryu to aa animation reverrse thatu hse brbr but pachu leave kari trt enter karyu toooo already amuk part animate thayela j che jem ke 2 section na h4 aai gya baki na lavana che to tya thij continue karooo fari pehle thi animation karso to overwrite thase
@@ -55,7 +59,7 @@ function navAnimation() {
         endTL.restart();
       else if (!endTL) {
         endTL = gsap.timeline();
-        endTL.to(".navelem h5", {
+        endTL.to(".navelem h5 span", {
           opacity: 0,
           display: "none",
           y: 35,
@@ -65,6 +69,10 @@ function navAnimation() {
           duration: 0.5,
           ease: "power1.out",
         });
+        endTL.to(".navelem h5",{
+          display:"none",
+          duration:0,
+        })
         endTL.to(".nav-bottom",{
             duration: 0.5,
             height: "0vh",
@@ -75,37 +83,183 @@ function navAnimation() {
     }
   });
 }
-navAnimation();
-
-var elems=document.querySelectorAll(".right-elem");
-
-elems.forEach((elem)=>{
-  elem.addEventListener("mouseenter",()=>{
-    gsap.to(elem.childNodes[3],{
+function page2Animation() {
+  var elems=document.querySelectorAll(".right-elem");
+  
+  elems.forEach((elem)=>{
+    elem.addEventListener("mouseenter",()=>{
+      gsap.to(elem.childNodes[3],{
+        opacity:1,
+        scale:1,
+        duration:0.2,
+      })
+    })
+  
+    elem.addEventListener("mouseleave",()=>{
+      gsap.to(elem.childNodes[3],{
+        opacity:0,
+        scale:0,
+        duration:0.3,
+      })
+    })
+    elem.addEventListener("mousemove",(dets)=>{
+      gsap.to(elem.childNodes[3],{
+        x:dets.x - elem.getBoundingClientRect().x-70,
+        y:dets.y - elem.getBoundingClientRect().y-140,
+        duration:0.4,
+      })
+    })
+      
+  })
+  
+  // getbounding react ee div che ene rectangular shape che enu detail moklave and ene dets.x thi minus karvathi image che ee pointer ni ghanin= najik aai jay
+  // dets.x - elem.getBoundingClientRect().x aavu ne ema -70 karyu to image ekdam middle ma aai gai
+}
+function page3Animation() {
+  var play=document.querySelector(".play i");
+  var video=document.querySelector(".page3 video");
+  play.addEventListener("mouseenter",()=>{
+    gsap.to(".play h4",{
       opacity:1,
-      scale:1,
-      duration:0.2,
+      transform:"translateY(0px)"
     })
   })
-
-  elem.addEventListener("mouseleave",()=>{
-    gsap.to(elem.childNodes[3],{
+  play.addEventListener("mouseleave",()=>{
+    gsap.to(".play h4",{
       opacity:0,
-      scale:0,
-      duration:0.3,
+      transform:"translateY(15px)"
     })
   })
-  elem.addEventListener("mousemove",(dets)=>{
-    console.log(elem.getBoundingClientRect()," and: ",dets.x);
-    console.log(dets.x - elem.getBoundingClientRect().x, "final output");
-    gsap.to(elem.childNodes[3],{
-      x:dets.x - elem.getBoundingClientRect().x-70,
-      y:dets.y - elem.getBoundingClientRect().y-140,
-      duration:0.4,
+  
+  play.addEventListener("click",()=>{
+    video.play();
+    gsap.to(video,{
+      transform:"scaleX(1) scaleY(1)",
+      borderRadius:"0px",
+      opacity:1,
+      duration:1,
     })
   })
-    
+  
+  video.addEventListener("click",()=>{
+    video.load(); //load krva thi jyr fari play par click karis to akho video fari chalu thse and jya thi chhodyo che tya thi chalu karvo hoy to use load instead of pause
+    gsap.to(video,{
+      transform:"scaleX(0.7) scaleY(0)",
+      borderRadius:"50px",
+      opacity:0,
+      duration:1.2,
+    })
+  })
+}
+function page5Animation() {
+  var sections=document.querySelectorAll(".section-right")
+sections.forEach((section)=>{
+  console.log(section.children);
+  // var image=section.children[0]; khabar nai kem but img pr event handler kam nthi kri rahyu
+  var video=section.children[1];
+  section.addEventListener("mouseenter",()=>{
+  //  console.log("helo");
+  video.style.opacity=1;
+  video.play();
+  })
+  section.addEventListener("mouseleave",()=>{
+  //  console.log("bye");
+  video.style.opacity=0;
+  video.pause();
+  video.currentTime=0;
+
+  })
+})
+}
+navAnimation();
+page2Animation();
+page3Animation();
+page5Animation();
+var cards = document.querySelectorAll(".infocard");
+cards.forEach((card) => {
+  var mask = card.querySelector(".mask");
+
+  card.addEventListener("mouseenter", () => {
+    gsap.fromTo(mask,{ 
+      y: "-100%",
+      opacity: 0 
+    }, // Start above and invisible
+    {
+      y: "0%",
+      opacity: 1,
+      duration: 0.4,
+      ease: "power2.out"
+    } // Move into view and become visible
+    );
+  });
+
+  card.addEventListener("mouseleave", () => {
+    gsap.to(mask, {
+      y: "100%", // Move back above
+      opacity: 0, // Fully invisible
+      duration: 0.35,
+      ease: "power2.in",
+    });
+  });
+});
+
+
+let uiux=document.querySelector(".uiux");
+let summary=document.querySelector(".uiux summary");
+let arrow=document.querySelector(".uiux h1 i");
+let flag=1
+summary.addEventListener("click",()=>{
+  if(flag==1){
+    gsap.to(uiux,{
+      borderTop:"2px solid #333",
+      duration:.5
+    })
+    gsap.to(arrow,{
+      rotate:180,
+      duration:.5
+    })
+   
+    flag=0
+  }
+  else{
+    gsap.to(uiux,{
+      borderTop:"2px solid #fff",
+      duration:.5
+    })
+    gsap.to(arrow,{
+      rotate:0,
+      duration:.5
+    })
+    flag=1
+  }
 })
 
-// getbounding react ee div che ene rectangular shape che enu detail moklave and ene dets.x thi minus karvathi image che ee pointer ni ghanin= najik aai jay
-// dets.x - elem.getBoundingClientRect().x aavu ne ema -70 karyu to image ekdam middle ma aai gai
+
+let product=document.querySelector("#product");
+let summary2=document.querySelector("#product summary");
+let arrow2=document.querySelector("#product h1 i");
+let mark=0
+summary2.addEventListener("click",()=>{
+  if(mark==1){
+    gsap.to(product,{
+      borderTop:"2px solid #333",
+      duration:.5
+    })
+    gsap.to(arrow2,{
+      rotate:180, //fari uper lai ja
+      duration:.5
+    })
+    mark=0
+  }
+  else{
+    gsap.to(product,{
+      borderTop:"2px solid #fff",
+      duration:.5
+    })
+    gsap.to(arrow2,{
+      rotate:0, //means jevu hatu evu kari de
+      duration:.5
+    })
+    mark=1
+  }
+})
